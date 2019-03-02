f59c51a56131b3bf4dea81a8c2dcc0c443efa5cf
go2rob <robmail1996@gmail.com>
2019-03-02 12:18:24 +0530

    regular dropdown implementation

--- src/containers/mainInterface/index.js
+++ src/containers/mainInterface/index.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
-import Dropdown from "../../widgets/dropDown/"
+import Dropdown from "../../widgets/dropDown"
+import { mockedData } from "../../constants"
 import './mainInterface.css';
 
+
 class MainInterface extends Component {
+  constructor(props) {
+    super(props)
+    this.dropdownItems = mockedData.array
+    this.state = {
+      selectedItem: this.dropdownItems[0]
+    }
+  }
+
+  handleSelect = (item, index) => {
+    console.log(item, index);
+    
+    this.setState(_prevState => ({
+      selectedItem: item
+    }))
+  }
+
   render() {
     return (
       <div className="main-container">
         <div className="sub-container">
           <Info text="Regular Dropdown"/>
-          <Dropdown/>
+          <Dropdown
+            data={this.dropdownItems}
+            selected={this.state.selectedItem}
+            onSelect={this.handleSelect}
+          />
         </div>
-        <div className="sub-container">
+        {/* <div className="sub-container">
           <Info text="Grouped Dropdown"/>
           <Dropdown/>
-        </div>
+        </div> */}
       </div>
     );
   }
@@ -22,10 +44,4 @@ class MainInterface extends Component {
 export default MainInterface;
 
 
-const Info = ({ text }) => {
-  return (
-    <div>
-      <span>{text}</span>
-    </div>
-  )
-}
+const Info = ({ text }) => (<div>{text}</div>)
--- src/widgets/dropDown/index.js
+++ src/widgets/dropDown/index.js
@@ -1,16 +1,107 @@
 import React, { Component } from 'react';
 import './dropDown.css';
+import PropTypes from "prop-types" 
+import _ from "lodash";
 
 class Dropdown extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedValue: this.getSelectedValue(),
+      isExpanded: false,
+      focusedIndex: -1
+      // TODO: change to false
+    }
+  }
+
+  getSelectedValue = () => {
+    return this.props.selected || this.props.data[0]
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      ...prevState,
+      isExpanded: !prevState.isExpanded
+    }))
+  }
+
+  handleMouseEnter = index => {
+    this.setState(prevState => ({
+      ...prevState,
+      focusedIndex: index
+    }))
+  }
+
+  handleMouseLeave = () => {
+    this.setState(prevState => ({
+      ...prevState,
+      focusedIndex: -1
+    }))
+  }
+
+  handleItemSelect = () => {
+    const { focusedIndex } = this.state
+    const item = this.props.data[focusedIndex]
+    this.handleClick()
+    this.props.onSelect(item, focusedIndex)
+  }
+
   render() {
     return (
       <div className="dropdown-main">
-        <div className="dropdown-display">
-          <span>dropdown comes here</span>
+        <div
+          className="dropdown-display"
+          onClick={this.handleClick}
+        >
+          <span>{this.getSelectedValue()}</span>
         </div>
+        <DropDownContainer
+          { ...this.props }
+          { ...this.state }
+          handleMouseEnter={this.handleMouseEnter}
+          handleMouseLeave={this.handleMouseLeave}
+          handleItemSelect={this.handleItemSelect}
+        />
       </div>
     );
   }
 }
 
 export default Dropdown;
+
+
+Dropdown.propTypes = {
+  data: PropTypes.oneOfType([
+    PropTypes.array,
+    PropTypes.object
+  ]).isRequired
+}
+
+const DropDownContainer = ({ data, onSelect, handleItemSelect, isExpanded, focusedIndex, handleMouseEnter, handleMouseLeave }) => {
+  if (!isExpanded) {
+    return null;
+  }
+  return (
+    <div className="dropdown-container">
+      {
+        _.map(data, (item, index) => {
+          const isFocused = focusedIndex===index
+          return (
+            <div
+              key={index}
+              className={isFocused ? "dropdown-item-focused" :"dropdown-item"}
+              onMouseEnter={() => handleMouseEnter(index)}
+              onMouseLeave={handleMouseLeave}
+              // onClick={() => onSelect(item, index)}
+              onClick={handleItemSelect}
+            >
+              <span>
+                {item}
+              </span>
+            </div>
+          )
+        })
+      }     
+    </div>
+  )
+}
